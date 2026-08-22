f021a46333075886df88273960c206c2d5ee2742
shrajan <149652391+shrajxn@users.noreply.github.com>
2026-08-22 20:39:37 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for r in range(9):
+            seen=set()
+            for c in range(9):
+                if board[r][c]==".":
+                    continue
+                if board[r][c] in seen:
+                    return False
+                seen.add(board[r][c])
+        for c in range(9):
+            seen=set()
+            for r in range(9):
+                if board[r][c]==".":
+                    continue
+                if board[r][c] in seen:
+                    return False
+                seen.add(board[r][c])
+        for square in range(9):
+            seen=set()
+            for i in range(3):
+                for j in range(3):
+                    row=(square//3)*3+i
+                    col=(square%3)*3+j
+                    if board[row][col]==".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+        return True
